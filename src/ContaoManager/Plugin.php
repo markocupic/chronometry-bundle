7539d7f0768cfc1a8104a1d2cbb80f958e9afbf2
Marko Cupic <m.cupic@gmx.ch>
2019-04-20 19:19:10 +0200

    dev

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @copyright  Marko Cupic 2019 <m.cupic@gmx.ch>
+ * @author     Marko Cupic
+ * @package    ChronometryBundle
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/markocupic/chronometry-bundle
+ *
+ */
+namespace Markocupic\ImportFromCsvBundle\ContaoManager;
+
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+
+/**
+ * Plugin for the Contao Manager.
+ *
+ * @author Marko Cupic
+ */
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create('Markocupic\ChronometryBundle\MarkocupicChronometryBundle')
+                ->setLoadAfter(['Contao\CoreBundle\ContaoCoreBundle']),
+        ];
+    }
+}
---
+++ src/MarkocupicChronometryBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Markocupic\ChronometryBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Configures the chronometry bundle.
+ *
+ * @author Marko Cupic <https://github.com/markocupic>
+ */
+class MarkocupicChronometryBundle extends Bundle
+{
+}
